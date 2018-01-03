b5724c7e9405ed1a8e29fc5205d54f18a106e871
Joaquim Noto <jnoto@systelabsw.com>
2018-01-03 11:25:57 +0100

    First version of application moved into github

---
+++ models/genre.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: {type: String, max: 100, min:3}
+
+});
+
+// Virtual for bookinstance's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
